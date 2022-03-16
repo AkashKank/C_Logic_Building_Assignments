b915c7a1b38d30369abd952b07f4977d292455ad
AkashKank <akashkank4@gmail.com>
2022-03-16 23:54:54 +0530

    Assignment 03

---
+++ Assignment 03/02 Write a Program Which Accept One Number From User And Print Factors Of That Number.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void Display_Factors(int iNo)
+{
+   int i=0;
+
+  if(iNo<=0)
+   {
+     iNo=-iNo;
+   }
+
+  printf("\nFactors Of %d: ",iNo);
+
+  for(i=1;i<=iNo/2;i++)
+   {
+
+     if(iNo%i==0)
+      {
+        printf("%d ",i);
+      }
+   }
+
+  return;
+}
+
+int main()
+{
+   int iValue=0;
+
+   printf("Enter Number: ");
+   scanf("%d",&iValue);
+
+   Display_Factors(iValue);
+
+  return 0;
+}
