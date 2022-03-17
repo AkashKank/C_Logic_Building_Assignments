363d6192c6c8a746aa4c73c58cc37170582c3d25
AkashKank <akashkank4@gmail.com>
2022-03-17 23:02:18 +0530

    Assignment 03

---
+++ Assignment 03/03 Write a Program Which Accept One Number From User And Print Even Factors Of That Number.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void Display_Even_Factors(int iNo)
+{
+   int i=0;
+
+  if(iNo<=0)
+   {
+     iNo=-iNo;
+   }
+
+  printf("\nEven Factors Of %d: ",iNo);
+
+  for(i=1;i<=iNo/2;i++)
+   {
+
+     if(iNo%i==0&&i%2==0)
+      {
+        printf("%d ",i);
+      }
+   }
+
+  return;
+}
+
+int main()
+{
+   int iValue=0;
+
+   printf("Enter Number: ");
+   scanf("%d",&iValue);
+
+   Display_Even_Factors(iValue);
+
+  return 0;
+}
