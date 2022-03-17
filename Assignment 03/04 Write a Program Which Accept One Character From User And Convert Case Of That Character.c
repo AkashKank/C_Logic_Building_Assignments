7eb4127244a5718a23a89aa1483ae4fb4e316533
AkashKank <akashkank4@gmail.com>
2022-03-17 23:03:16 +0530

    Assignment 03

---
+++ Assignment 03/04 Write a Program Which Accept One Character From User And Convert Case Of That Character.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void Display_Convert(char CValue)
+{
+
+  if(CValue>=65&&CValue<=90)
+   {
+     printf("\nOutput: %c\n",CValue+32);
+   }
+  else if(CValue>=97&&CValue<=122)
+   {
+     printf("\nOutput: %c\n",CValue-32);
+   }
+
+
+  return;
+}
+
+int main()
+{
+   char cValue='\0';
+
+   printf("Enter Character: ");
+   scanf("%c",&cValue);
+
+   Display_Convert(cValue);
+
+  return 0;
+}
